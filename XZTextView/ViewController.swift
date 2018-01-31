399e4f74464c656eb85504d24efb16b61368df53
ios_domain <1935722630@qq.com>
2018-01-31 14:35:30 +0800

    Initial Commit

---
+++ XZTextView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XZTextView
+//
+//  Created by admin on 2018/1/31.
+//  Copyright © 2018年 XZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
